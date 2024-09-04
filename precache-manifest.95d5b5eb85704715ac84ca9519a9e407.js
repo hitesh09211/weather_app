c00aa1366820fab97327350a93033fdb1096d562
hitesh kumar <hiteshchouhan9680@gmail.com>
2024-09-04 18:31:34 +0530

    Updates

---
+++ precache-manifest.95d5b5eb85704715ac84ca9519a9e407.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b02834c62175d976d7fcc78f3c77b92",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "acccc4a57b75c3822588",
+    "url": "/weather_app/static/css/main.e3a03f6a.chunk.css"
+  },
+  {
+    "revision": "094cb5df1fd5cd6c5884",
+    "url": "/weather_app/static/js/2.6150fb03.chunk.js"
+  },
+  {
+    "revision": "acccc4a57b75c3822588",
+    "url": "/weather_app/static/js/main.cb6c2145.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather_app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/weather_app/static/media/city.e1698332.jpg"
+  }
+]);
